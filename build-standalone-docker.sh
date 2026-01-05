c157a7d702ea16ab12113ff17982f3dbcc3c80dd
Gord Lea <jgordonlea@gmail.com>
2026-01-05 07:12:22 -0800

    chore: update standalone build scripts

--- build-standalone-docker.sh
+++ build-standalone-docker.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-docker buildx build --platform linux/amd64,linux/amd64/v2,linux/arm64,linux/arm/v7 -t gordlea/fireboard2mqtt:latest 
+docker buildx build --platform linux/amd64,linux/amd64/v2,linux/amd64/v3,linux/arm64,linux/arm/v7 -t gordlea/fireboard2mqtt:latest -f ./docker/standalone.Dockerfile .
+
