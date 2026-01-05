8d8c9fdfcd8b7890b76cb4b79f02f55e8af50b7a
Gord Lea <jgordonlea@gmail.com>
2026-01-05 06:35:33 -0800

    fix: remove obsolete channel_count property from device (#69)
    * chore: cleanup
    
    * fix: field channel_count no longer exists in api

--- src/fireboard_api.rs
+++ src/fireboard_api.rs
@@ -27,12 +27,6 @@ pub struct FireboardCloudApiAuthResponse {
     pub(crate) key: String,
 }
 
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct FireboardDeviceList {
-    pub devices: Vec<FireboardApiDevice>,
-}
-
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct FireboardApiDevice {
     pub id: usize,
@@ -40,7 +34,6 @@ pub struct FireboardApiDevice {
     pub title: String,
     pub hardware_id: String,
     pub version: String,
-    pub channel_count: usize,
     pub degreetype: DegreeType,
     pub model: String,
     pub channels: Vec<FireboardDeviceChannel>,
